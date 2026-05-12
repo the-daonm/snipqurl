5b468cc7aeae5bd0f089823763268efc4f14aec8
daonm <daobilionusd@gmail.com>
2026-05-12 16:58:45 +0700

    fix: improve favicon compatibility and app visibility

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,10 +9,12 @@ import (
 func SetUp(h *handler.URLHandler) *gin.Engine {
 	r := gin.Default()
 
-	r.Static("/static", "./static")
+	// Serve static files
 	r.StaticFile("/", "./static/index.html")
 	r.StaticFile("/favicon.ico", "./static/favicon.ico")
+	r.Static("/static", "./static")
 
+	// API routes
 	api := r.Group("/api")
 	{
 		api.POST("/shorten", h.Shorten)
