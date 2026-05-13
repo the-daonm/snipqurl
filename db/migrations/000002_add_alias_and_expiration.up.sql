ca3877b494345745ab86ff322ff45fa455673a92
daonm <daobilionusd@gmail.com>
2026-05-13 19:11:04 +0700

    fix: update database migration in production with k8s and argocd

--- db/migrations/000002_add_alias_and_expiration.up.sql
+++ db/migrations/000002_add_alias_and_expiration.up.sql
@@ -1,3 +1,3 @@
 -- Alter short_code length and add expires_at column
 ALTER TABLE urls ALTER COLUMN short_code TYPE varchar(30);
-ALTER TABLE urls ADD COLUMN expires_at timestamp;
+ALTER TABLE urls ADD COLUMN IF NOT EXISTS expires_at timestamp;
